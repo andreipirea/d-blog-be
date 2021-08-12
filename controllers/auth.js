0228874adb6362fb8a81d84cb143197e342bdd0e
Andrei Pirea <andrei.pirea@hogarthww.com>
2021-08-12 17:49:22 +0300

    signup in progress

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,32 +4,43 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 exports.signup = async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const error = new Error("Validation failed");
-    error.statuscode = 422;
-    throw error;
-  }
+  let userExists;
+  let selectSql = `SELECT * FROM users WHERE  email = '${req.body.email}'`;
+  await db.query(selectSql, async (err, result) => {
+    if (result.length !== 0) {
+      userExists = result[0];
+      const error = new Error("Exista deja un user cu acest email");
+      error.statusCode = 401;
+      next(error);
+    }
+  });
+
+  
   const password = req.body.password;
+  const salt = await bcrypt.genSaltSync();
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-  try {
-    const salt = await bcrypt.genSaltSync();
-    const hashedPassword = await bcrypt.hash(password, salt);
+  const user = {
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword
+  };
 
-    const user = {
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPassword
-    };
+  const token = jwt.sign(
+    {
+      email: user.email,
+      password: user.password
+    },
+    "somesupersecretsecret",
+    { expiresIn: "1h" }
+  );
 
-    let sql = "INSERT INTO users SET ?";
-    db.query(sql, user, (err, result) => {
-      if (err) throw err;
-      res.send(result);
-    });
-  } catch (err) {
-    throw err;
-  }
+  let insertSql = "INSERT INTO users SET ?";
+  db.query(insertSql, user, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.status(200).json({ token: token, userId: result.insertId.toString() });
+  });
 };
 
 exports.login = (req, res, next) => {
@@ -46,17 +57,20 @@ exports.login = (req, res, next) => {
       next(error);
     }
     foundUser = result[0];
-    
+
     const isEquel = await bcrypt.compare(password, foundUser.password);
 
-    const token = jwt.sign({
-      email: foundUser.email,
-      password: foundUser.password
-    }, 'somesupersecretsecret', {expiresIn: '1h'});
+    const token = jwt.sign(
+      {
+        email: foundUser.email,
+        password: foundUser.password
+      },
+      "somesupersecretsecret",
+      { expiresIn: "1h" }
+    );
 
-    if (isEquel) {
-      res.status(200).json({token: token, userId: foundUser.id.toString()});
+    if (isEquel && !userExists) {
+      res.status(200).json({ token: token, userId: foundUser.id.toString() });
     }
-    
   });
 };
