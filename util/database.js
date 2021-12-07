bad35294079295be7d37fff146e0ceaa803d1188
andreipirea <andrei.pirea@yahoo.ro>
2021-12-07 17:06:46 +0200

    new heroku db

--- util/database.js
+++ util/database.js
@@ -1,19 +1,19 @@
 const mysql = require("mysql");
 
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "d-blog"
-});
-
 // const pool = mysql.createPool({
-//   host: "eu-cdbr-west-01.cleardb.com",
-//   user: "b0879c2b93cbb7",
-//   password: "4d89a313",
-//   database: "heroku_4187931d1ef1174"
+//   host: "localhost",
+//   user: "root",
+//   password: "root",
+//   database: "d-blog"
 // });
+
+const pool = mysql.createPool({
+  host: "eu-cdbr-west-01.cleardb.com",
+  user: "b7966ec5a73a73",
+  password: "70b71ebc",
+  database: "heroku_e8ce7f018aca5a4"
+});
  
 module.exports = pool;
 
