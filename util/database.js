85e6742983421cfdf7de6f22ed3b4ef717f30b17
andreipirea <andrei.pirea@yahoo.ro>
2024-02-16 12:40:37 +0200

    updated gitignore, database.js

--- util/database.js
+++ util/database.js
@@ -1,19 +1,19 @@
 const mysql = require("mysql");
 
 
-// const pool = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   password: "root",
-//   database: "d-blog"
-// });
-
 const pool = mysql.createPool({
-  host: "eu-cdbr-west-01.cleardb.com",
-  user: "b7966ec5a73a73",
-  password: "70b71ebc",
-  database: "heroku_e8ce7f018aca5a4"
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "d-blog"
 });
+
+// const pool = mysql.createPool({
+//   host: "eu-cdbr-west-01.cleardb.com",
+//   user: "b7966ec5a73a73",
+//   password: "70b71ebc",
+//   database: "heroku_e8ce7f018aca5a4"
+// });
  
 module.exports = pool;
 
